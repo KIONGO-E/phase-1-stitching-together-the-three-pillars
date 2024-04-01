be7d65101a21b93c689da2f687f1e465e56ae41b
ElvisKiongo <elvis.kiongo@student.moringaschool.com>
2024-04-01 14:56:02 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 /* This is a file inserted to keep the learn IDE browser happy */
 const testVar = {}
+require('./helpers.js');
+
+describe("index.js", () => {
+  it("exists", () => {
+    expect(testVar).to.exist
+    expect(testVar).to.deep.equal({}) 
+  })
+})
